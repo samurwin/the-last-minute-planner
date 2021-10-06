c064c770212dafe1c74d64564259582df16e3589
smudesign <urwinsamantha@gmail.com>
2021-10-06 13:46:25 -0400

    created function that gets api data for displaying events

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,11 +19,10 @@ $('#searchBtn').on('click', function(event) {
     // call the ticket master api using the date and cityName (fetch)
     fetch('https://app.ticketmaster.com/discovery/v2/events.json?size=10&city=' + cityName + '&date=' + date +'&apikey=GLE8iclmKIizOPTZtUoLOFpHe2fHejvM')
     .then(function(data) {
-        data.json().then(function(data) {
-            console.log(data);
-            // then call displayEvents(data)
-            // displayEvents(data);
-        })
+        return data.json();
+    })
+    .then(function(data) {
+        displayEvents(data)
     });
     // call the weather api using the date and cityName (fetch)
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=460baac12caacdeca58e7bae8f1299bc')
@@ -33,12 +32,12 @@ $('#searchBtn').on('click', function(event) {
     .then(function(response) {
         fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + response.coord.lat + '&lon=' + response.coord.lon + '&date=' + date +'&units=metric&appid=460baac12caacdeca58e7bae8f1299bc')
         .then(function(weather) {
-            console.log(weather.json());
             return weather.json();
         })
         // then call displayWeather(data)
         .then(function(weather) {
             // displayWeather(weather);
+            console.log(weather);
         })
     })
 });
@@ -47,38 +46,69 @@ $('#searchBtn').on('click', function(event) {
     // text of the span will equal the cityName + date
 
 // create a function to display the events on the page
+var displayEvents = function(data) {
+    console.log(data);
     // create variable for #results container using query selector
+    var resultsContainerEl = document.getElementById('#results');
 
     // create a for loop to iterate through the events happening on that day
+    for (i = 0; i < data._embedded.events.length; i ++) {
         // create a div to hold the event info
+        var eventContainerEl = $('<div></div>')
         // add classes
+        .addClass('col-11 border-dark p-2 m-2');
         // create a div to hold the event name, venue, and "add to favourites"
+        var eventTitleContainerEl = $('<div></div>')
         // add classes
+        .addClass('d-flex flex-row align-items-center justify-content-between');
         // create <h4></h4> for the event name
+        var eventTitleEl = $('<h4></h4>')
         // text will equal data.event-name
+        .text(data._embedded.events[i].name);
         // add classes
         // create a <p></p> for the Venue
+        var venueEl = $('<p></p>')
         // text will equal data.venue
+        .text(data._embedded.events[i]._embedded.venues[0].name)
         // add classes
+        .addClass('text-muted');
         // create a <a></a> for "add to favourites"
+        var favouritesLinkEl = $('<a></a>')
         // text will equal "add to favourites"
+        .text("Add To Favourites")
         // add classes
+        .addClass('text-decoration-underline');
 
         // append eventName, venue, addToFavourites to the rowDiv
+        $(eventTitleContainerEl).append(eventTitleEl, venueEl, favouritesLinkEl);
         
         // create a <p></p> for the start time
+        var startTimeEl = $('<p></p>')
         // text will equal data.start-time
+        .text(data._embedded.events[i].dates.start.localTime)
         // add classes
+        .addClass('font-weight-light');
         // create a <p></p> for the description
+        // var descriptionEl = $('<p></p>')
         // text will equal data.description
+        // .text(events._embedded.events[i].classification[0].genre.name)
         // add classes
         // create a <a></a> for "more info"
+        var moreInfoEl = $('<a></a>')
         // text will equal "More Info"
-        // add classes
+        .text("More Info")
+        // add link
+        .attr('src', data._embedded.events[i].url)
 
         // append rowDiv, startTime, description, moreInfo to div
+        $(eventContainerEl).append(eventTitleContainerEl, startTimeEl, moreInfoEl);
 
         // append div to #results
+        $(resultsContainerEl).append(eventContainerEl);
+    }
+
+};
+
 
 // create a function to display the weather
     // create a variable for the #forecast container
